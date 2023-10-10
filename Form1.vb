2a8f3a00b272a7afb74c6bf60ef400f07ab9e9b2
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 08:37:50 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim A As Double
+        Dim pi As Double = 3.14
+        Dim r As Double
+
+
+        r = Val(TextBox1.Text)
+        A = pi * r ^ 2
+
+        TextBox2.Text = A
+    End Sub
+End Class
